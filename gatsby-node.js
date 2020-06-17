f0196ecaf31eeb3c0908fd7b6f06065026485062
Cole J Calamos <colejcalamos@gmail.com>
2020-06-17 14:44:22 -0500

    Updated gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,17 @@
-"use strict";var _sourceNodes=_interopRequireDefault(require("./dist/sourceNodes"));Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"sourceNodes",{enumerable:!0,get:function(){return _sourceNodes.default}});function _interopRequireDefault(a){return a&&a.__esModule?a:{default:a}}
+"use strict";
+
+var _sourceNodes = _interopRequireDefault(require("./dist/sourceNodes"));
+
+Object.defineProperty(exports, "__esModule", {
+    value: !0,
+}),
+    Object.defineProperty(exports, "sourceNodes", {
+        enumerable: !0,
+        get: function() {
+            return _sourceNodes.default;
+        },
+    });
+
+function _interopRequireDefault(a) {
+    return a && a.__esModule ? a : { default: a };
+}
