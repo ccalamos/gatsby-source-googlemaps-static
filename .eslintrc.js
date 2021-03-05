b26567f8e1ed73aeb1a8ace97bc0d340841d50b2
Cole J Calamos <colejcalamos@gmail.com>
2021-03-05 16:11:23 -0600

    Fixed eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,6 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint",
+        "prettier",
     ],
 };
